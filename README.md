e724ed45007bef2064c4b14b6c1f3ee963c58489
Michał <kacprzakmichal99@gmail.com>
2019-06-18 21:12:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# vm_manage
+----
+Simple script to automate managing VMs.
+In order to managing VMs you must create a group file. Here is an examplary file:
+```
+        [group1]
+        vm1
+        vm2
+        vm3
+        [group2]
+        vm4
+        vm5
+        vm6
+```
+----
+## Options
+```-f groupfile``` - spcecify group file
+```-o start|shutdown|destroy``` - specify option
+```-m groupname``` - specify group name
+```-s``` - shutdown all VMs
+```-d``` - destroy all VMs
+```-h``` - show help
+
+## Examples
+```vm_manage.sh -f groupfile -o start -m group1``` - start all VMs in group1 that are specified in groupfile
+```vm_manage.sh -f groupfile -o shutdown -m group2``` - shutdown all VMs in group2
+```vm_manage.sh -s``` - shutdown all VMs
---
+++ vm_manage.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+#
+#----------------------------------------------------------
+#
+# Name: vm_manage.sh
+#
+# Author: Michał Kacprzak										
+#														
+# Description: Script to automate managaing VMs 
+#														
+#----------------------------------------------------------
+
+shutdown_all(){
+
+	operation="$1" # shutdown or destroy
+
+	for VM in $(virsh list --name)
+		do
+			virsh "$operation" "$VM" &> /dev/null
+		done
+
+}
+
+manage_group(){
+
+	domainfile="$1"
+	groupname="$2"
+	operation="$3"
+
+	if [[ ! -f "$domainfile" ]]; then
+		echo "Domain file does not exist"
+		exit 1
+	fi
+	
+	case "$operation"
+		in
+			start)
+				virsh_command="virsh start"
+				;;
+			shutdown)
+				virsh_command="virsh shutdown"
+				;;
+			destroy)
+				virsh_command="virsh destroy"
+				;;
+	esac
+
+	MATCH=false
+	while read line
+		do
+			if [[ "$line" =~ ^\[.*\]$ ]]; then
+				if [[ "$line" != "[$groupname]" ]]; then
+					MATCH=false
+				else
+					MATCH=true
+					continue
+				fi
+			fi
+
+			if [[ "$MATCH" = true ]]; then
+				$virsh_command "$line" &> /dev/null
+			fi
+		done < "$domainfile"
+}
+
+show_help(){
+	echo "Syntax: $(basename $0) [OPTION]"
+	echo "-f specify group file"
+	echo "-o specify option: start|shutdown|destroy"
+	echo "-m specify group name"
+	echo "-s shutdown all VMs"
+	echo "-d destroy all VMs"
+	echo "-h show help"
+
+}
+
+## MAIN BLOCK ##
+
+if [[ $(id -u) -ne 0 ]]; then
+	echo "You must run script as root"
+	exit 1
+fi
+
+while getopts "f: o: m: s d h" option
+	do
+		case "${option}"
+			in
+				f) 	file="$OPTARG";;
+				o)	operation="$OPTARG";;
+				m)	manage_group  "$file" "$OPTARG" "$operation";;
+				s)	shutdown_all shutdown;;
+				d)	shutdown_all destroy;;
+				h) 	show_help;;
+				*)	show_help;;
+		esac
+	done
